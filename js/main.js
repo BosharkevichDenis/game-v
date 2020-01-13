b86906a1ad48ff5e89cf60a4a572ddb49e64e39e
BosharkevichDenis <denisbosh2@gmail.com>
2020-01-13 13:03:23 +0200

    learning

--- js/main.js
+++ js/main.js
@@ -11,16 +11,45 @@
 // let seee = "list-style-image";
 // camelize(seee);
 
-function filterRange(arr, a, b) {
-let brr = arr.map(function(num, index, arr) {
-   if(num > a && num < b) {
-    
-    arr.push(num);
-  }
- 
-});
- console.log(brr);
-}
 
-let array = [1,4,5,8,3,4,2];
-filterRange(array, 3, 6);
+
+// function filterRange(arr, a, b) {
+// let brr = arr.filter(num => num > a && num < b) 
+//  console.log(brr);
+// }
+
+// let array = [1,4,5,8,3,4,2];
+// filterRange(array, 1, 6);
+
+// function filterRangeInPlace(arr, a, b) {
+//   arr.forEach(function(num, index){
+//     if (num > a && num < b) {}
+//       else{
+//          arr.splice(index, 1);
+//       }
+//   }) 
+//   console.log(arr)
+// }
+// let array = [1,4,5,8,3,4,2, 3,3,3,3,3,3,3];
+
+// filterRangeInPlace(array, 2, 6)
+
+// let arr = [5, 2, 1, -10, 8];
+// arr.sort();
+// arr.reverse();
+
+// alert(arr);
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// function copySorted(array) {
+  
+//   return array.slice().sort();
+// }
+
+
+// let sorted = copySorted(arr);
+
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
