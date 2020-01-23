e7c2a5cba4c6f65c09fe1146c9ba7cb6e0d525af
BosharkevichDenis <denisbosh2@gmail.com>
2020-01-23 21:59:58 +0200

    learning

--- js/main.js
+++ js/main.js
@@ -156,4 +156,44 @@
   
 // };
 
+// alert( count(user) ); // 
 // alert( count(user) ); // 2
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+
+// alert( sumSalaries(salaries) ); // 650
+
+// function count(obj) {
+//    return (Object.keys(obj)).length;
+// }
+
+// let user = {
+
+//   age: 30
+  
+// };
+
+// alert( count(user) ); // 
+// alert( count(user) ); // 2
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+
+// alert( sumSalaries(salaries) ); // 650
+
+// function count(obj) {
+//    return (Object.keys(obj)).length;
+// }
+
+// let user = {
+
+//   age: 30
+  
+// };
+
+// alert( count(user) ); // 
